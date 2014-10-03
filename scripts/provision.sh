aa00007ed488eec0162afd9fe633b27d92db2713
Taylor Otwell <taylorotwell@gmail.com>
2014-10-02 19:45:16 -0500

    Push HHVM changes.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -83,6 +83,23 @@ rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 service nginx restart
 
+# Add The HHVM Key & Repository
+
+wget -O - http://dl.hhvm.com/conf/hhvm.gpg.key | apt-key add -
+echo deb http://dl.hhvm.com/ubuntu trusty main | tee /etc/apt/sources.list.d/hhvm.list
+apt-get update
+apt-get install -y hhvm
+
+# Configure HHVM To Run As Homestead
+
+service hhvm stop
+sed -i 's/#RUN_AS_USER="www-data"/RUN_AS_USER="vagrant"/' /etc/default/hhvm
+service hhvm start
+
+# Start HHVM On System Start
+
+update-rc.d hhvm defaults
+
 # Setup Some PHP-FPM Options
 
 ln -s /etc/php5/mods-available/mailparse.ini /etc/php5/fpm/conf.d/20-mailparse.ini
