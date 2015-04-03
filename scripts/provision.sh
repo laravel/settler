35015e24c8771562b2b5e5738c76bbc8c2294262
Mark van Eijk <mark@vormkracht10.nl>
2015-04-03 17:37:08 +0200

    Change upload max size in PHP
    The nginx configuration is now set to maximum upload 100 mb files, but PHP config is still on 2 mb. So the php.ini needs to be configurated too. With one single sed command this is possible.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -79,6 +79,7 @@ sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/cli/php.
 sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/cli/php.ini
 sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php5/cli/php.ini
 sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php5/cli/php.ini
+sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php5/cli/php.ini
 
 # Install Nginx & PHP-FPM
 
@@ -113,6 +114,7 @@ sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/fpm/php.ini
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/fpm/php.ini
 sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php5/fpm/php.ini
 sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php5/fpm/php.ini
+sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php5/fpm/php.ini
 sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php5/fpm/php.ini
 
 echo "xdebug.remote_enable = 1" >> /etc/php5/fpm/conf.d/20-xdebug.ini
