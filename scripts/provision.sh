ddba4d2d3b1d96fd5743b9059cece9628771bbc7
Josh Frye <joshfng@gmail.com>
2015-06-22 08:35:45 -0400

    Remove two debug lines

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -7,8 +7,6 @@ apt-get update
 # Update Grub Bootloader
 
 echo "set grub-pc/install_devices /dev/sda" | debconf-communicate
-apt-get -y remove grub-pc
-apt-get -y install grub-pc
 grub-install /dev/sda
 update-grub
 
