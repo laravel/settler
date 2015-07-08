7a4fc2e687691a2a6cd6a69b33b661e30da23749
Jordan Pittman <jordan@cryptica.me>
2015-07-08 19:26:04 -0400

    Fix HHVM installation on Ubuntu 14.04 "Trusty" LTS

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -93,7 +93,7 @@ service nginx restart
 # Add The HHVM Key & Repository
 
 wget -O - http://dl.hhvm.com/conf/hhvm.gpg.key | apt-key add -
-echo deb http://dl.hhvm.com/ubuntu utopic main | tee /etc/apt/sources.list.d/hhvm.list
+echo deb http://dl.hhvm.com/ubuntu trusty main | tee /etc/apt/sources.list.d/hhvm.list
 apt-get update
 apt-get install -y hhvm
 
