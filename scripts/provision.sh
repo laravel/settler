31bcf396fa4d20e8e2222fafeb2b9692aac67ffa
Taylor Otwell <taylorotwell@gmail.com>
2015-11-12 11:54:31 -0600

    Revert "Upgrade mysql version to 5.7"

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -20,10 +20,6 @@ apt-add-repository ppa:nginx/stable -y
 apt-add-repository ppa:rwky/redis -y
 apt-add-repository ppa:ondrej/php5-5.6 -y
 
-# gpg: key 5072E1F5: public key "MySQL Release Engineering <mysql-build@oss.oracle.com>" imported
-apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 5072E1F5
-sh -c 'echo "deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7" >> /etc/apt/sources.list.d/mysql.list'
-
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" >> /etc/apt/sources.list.d/postgresql.list'
 
@@ -192,7 +188,7 @@ apt-get install -y sqlite3 libsqlite3-dev
 
 debconf-set-selections <<< "mysql-server mysql-server/root_password password secret"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password secret"
-apt-get install -y mysql-server
+apt-get install -y mysql-server-5.6
 
 # Configure MySQL Remote Access
 
