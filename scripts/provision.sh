f4ca4a05a5eceb4ab284fa01d98ee7982fa806d7
Taylor Otwell <taylorotwell@gmail.com>
2016-01-25 10:12:35 -0600

    add xdebug

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -52,7 +52,7 @@ ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 apt-get install -y --force-yes php7.0-cli php7.0-dev \
 php-pgsql php-sqlite3 php-gd php-apcu \
 php-curl php7.0-dev \
-php-imap php-mysql php-memcached php7.0-readline
+php-imap php-mysql php-memcached php7.0-readline php-xdebug
 
 # Install Composer
 
