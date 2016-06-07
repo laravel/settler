9ad04c61eed8bbb163b567d4f8f9015ad6636a46
Simon Kjellberg <simon.kjellberg@gmail.com>
2016-06-07 11:15:49 +0200

    Upgrade nodejs to v6

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -32,7 +32,7 @@ sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" >> /
 curl -s https://packagecloud.io/gpg.key | apt-key add -
 echo "deb http://packages.blackfire.io/debian any main" | tee /etc/apt/sources.list.d/blackfire.list
 
-curl --silent --location https://deb.nodesource.com/setup_5.x | bash -
+curl --silent --location https://deb.nodesource.com/setup_6.x | bash -
 
 # Update Package Lists
 
