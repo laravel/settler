1b50fb0c3bd3fd0a9cb304e35400d671316d590b
Joe Ferguson <joe@joeferguson.me>
2016-12-05 17:24:35 -0600

    :sparkles: :ribbon: Add MailHog for local Mail handling

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -233,6 +233,31 @@ apt-get install -y redis-server memcached beanstalkd
 sed -i "s/#START=yes/START=yes/" /etc/default/beanstalkd
 /etc/init.d/beanstalkd start
 
+# Install & Configure MailHog
+
+# Download binary from github
+wget --quiet -O /usr/local/bin/mailhog https://github.com/mailhog/MailHog/releases/download/v0.2.1/MailHog_linux_amd64
+
+# Make it executable
+chmod +x /usr/local/bin/mailhog
+
+# Make it start on reboot
+sudo tee /etc/systemd/system/mailhog.service <<EOL
+[Unit]
+Description=Mailhog
+After=network.target
+
+[Service]
+User=vagrant
+ExecStart=/usr/bin/env /usr/local/bin/mailhog > /dev/null 2>&1 &
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+# Start it now in the background
+service mailhog start
+
 # Configure Supervisor
 
 systemctl enable supervisor.service
