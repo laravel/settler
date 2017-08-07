c3694d3db468a07cb28577f04059ca3afe3b9f6a
Joe Ferguson <joe@joeferguson.me>
2017-08-06 20:32:16 -0500

    🚑 Add missing mcrypt from PHP 5.6 install

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -77,7 +77,7 @@ php5.6-pgsql php5.6-sqlite3 php5.6-gd \
 php5.6-curl php5.6-memcached \
 php5.6-imap php5.6-mysql php5.6-mbstring \
 php5.6-xml php5.6-zip php5.6-bcmath php5.6-soap \
-php5.6-intl php5.6-readline
+php5.6-intl php5.6-readline php5.6-mcrypt
 
 # Install Composer
 
