464f559e181818e117bb8b8138c134c9c4427628
Joe Ferguson <joe@joeferguson.me>
2017-11-02 20:09:11 -0500

    ✨ Add oh-my-zsh and set to default shell (#123)

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -398,6 +398,13 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
+# Install oh-my-zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
+cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc
+chown -R vagrant:vagrant /home/vagrant/.oh-my-zsh
+chown vagrant:vagrant /home/vagrant/.zshrc
+chsh -s /usr/bin/zsh vagrant
+
 # Clean Up
 
 apt-get -y autoremove
