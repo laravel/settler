7c0eb8083b6fdc49d6396b837a10f5d5669fcb83
Joe Ferguson <joe@joeferguson.me>
2017-12-06 07:55:02 -0600

    🚑 Fixing a configuration bug with Postgres. We should use md5 instead of peer

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -328,7 +328,7 @@ apt-get install -y postgresql
 # Configure Postgres Remote Access
 
 sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/9.5/main/postgresql.conf
-echo "host    all             all             10.0.2.2/32               md5" | tee -a /etc/postgresql/9.5/main/pg_hba.conf
+sed -i "s/peer/md5" /etc/postgresql/9.5/main/pg_hba.conf
 sudo -u postgres psql -c "CREATE ROLE homestead LOGIN UNENCRYPTED PASSWORD 'secret' SUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;"
 sudo -u postgres /usr/bin/createdb --echo --owner=homestead homestead
 service postgresql restart
