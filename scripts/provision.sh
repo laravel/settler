88910ee1bf6e531b97fbd600e219535bde57c21a
Joe Ferguson <joe@joeferguson.me>
2017-12-23 20:38:58 -0600

    ✨ Add Zend Z-ray

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -333,6 +333,14 @@ service postgresql restart
 
 apt-get install -y blackfire-agent blackfire-php
 
+# Install Zend Z-Ray
+
+sudo wget http://repos.zend.com/zend-server/early-access/ZRay-Homestead/zray-standalone-php72.tar.gz -O - | sudo tar -xzf - -C /opt
+sudo ln -sf /opt/zray/zray.ini /etc/php/7.2/cli/conf.d/zray.ini
+sudo ln -sf /opt/zray/zray.ini /etc/php/7.2/fpm/conf.d/zray.ini
+sudo ln -sf /opt/zray/lib/zray.so /usr/lib/php/20170718/zray.so
+sudo chown -R vagrant:vagrant /opt/zray
+
 # Install The Chrome Web Driver & Dusk Utilities
 
 apt-get -y install libxpm4 libxrender1 libgtk2.0-0 \
