170648b3e4022d76889c3a315b367d23a4ba5cb4
Norbert Fuksz <ciufy22@gmail.com>
2018-01-13 18:16:11 +0000

    Make flyway executable by anyone (#141)
    Flyway is only executable by root. This fixes that.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -395,6 +395,7 @@ rm -rf ngrok-stable-linux-amd64.zip
 
 wget https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/4.2.0/flyway-commandline-4.2.0-linux-x64.tar.gz
 tar -zxvf flyway-commandline-4.2.0-linux-x64.tar.gz -C /usr/local
+chmod +x /usr/local/flyway-4.2.0/flyway
 ln -s /usr/local/flyway-4.2.0/flyway /usr/local/bin/flyway
 rm -rf flyway-commandline-4.2.0-linux-x64.tar.gz
 
