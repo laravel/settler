313c5ea571b9435a212bb692810da58d2554b65d
Ricardo Seriani <ricardo.seriani@gmail.com>
2018-04-25 15:06:24 -0300

    Makes it easy to update the golang version

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -416,10 +416,11 @@ chown vagrant:vagrant /home/vagrant/.zshrc
 
 # Install Golang
 
-wget https://dl.google.com/go/go1.10.1.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.10.1.linux-amd64.tar.gz
+golangVersion="1.10.1"
+wget https://dl.google.com/go/go${golangVersion}.linux-amd64.tar.gz -O golang.tar.gz
+tar -C /usr/local -xzf golang.tar.gz
 printf "\nPATH=\"/usr/local/go/bin:\$PATH\"\n" | tee -a /home/vagrant/.profile
-rm -rf go1.10.1.linux-amd64.tar.gz
+rm -rf golang.tar.gz
 
 # Install & Configure Postfix
 
