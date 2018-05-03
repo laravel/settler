a146014277e15f6c9e3504315ffeb7801fe90255
Ricardo Seriani <ricardo.seriani@gmail.com>
2018-05-03 16:05:36 -0300

    Update golang version to 1.10.2 (#156)

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -416,7 +416,7 @@ chown vagrant:vagrant /home/vagrant/.zshrc
 
 # Install Golang
 
-golangVersion="1.10.1"
+golangVersion="1.10.2"
 wget https://dl.google.com/go/go${golangVersion}.linux-amd64.tar.gz -O golang.tar.gz
 tar -C /usr/local -xzf golang.tar.gz
 printf "\nPATH=\"/usr/local/go/bin:\$PATH\"\n" | tee -a /home/vagrant/.profile
