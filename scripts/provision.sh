cff699a0df1250ec52178e9ca92510eac0ca86cc
Joe Ferguson <joe@joeferguson.me>
2018-05-15 17:07:49 -0500

    ✨ Adding support for Crystal, Lucky Framework, and Heroku CLI

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -384,6 +384,28 @@ systemctl enable mailhog
 systemctl enable supervisor.service
 service supervisor start
 
+# Install Crystal Programming Language Support
+apt-key adv --keyserver hkp://keys.gnupg.net:80 --recv-keys 09617FD37CC06B54
+echo "deb https://dist.crystal-lang.org/apt crystal main" | tee /etc/apt/sources.list.d/crystal.list
+apt-get update
+apt-get install -y crystal libssl1.0-dev
+
+# Install Lucky Framework for Crystal
+
+wget https://github.com/luckyframework/lucky_cli/archive/v0.10.0-rc3.tar.gz
+tar -zxvf v0.10.0-rc3.tar.gz
+cd lucky_cli-0.10.0-rc3
+crystal deps
+crystal build src/lucky.cr --release --no-debug
+mv lucky /usr/local/bin/.
+cd /home/vagrant
+rm -rf lucky_cli-0.10.0-rc3
+rm -rf v0.10.0-rc3.tar.gz
+
+# Install Heroku CLI
+
+curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
+
 # Install ngrok
 
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
