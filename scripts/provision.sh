aa43fb55544c097e3a8f1fa740677d1ee8462b66
devosc <devosc@gmail.com>
2018-06-30 18:29:59 -0500

    add php7.2-ldap

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -58,7 +58,7 @@ php7.2-pgsql php7.2-sqlite3 php7.2-gd \
 php7.2-curl php7.2-memcached \
 php7.2-imap php7.2-mysql php7.2-mbstring \
 php7.2-xml php7.2-zip php7.2-bcmath php7.2-soap \
-php7.2-intl php7.2-readline \
+php7.2-intl php7.2-readline php7.2-ldap \
 php-xdebug php-pear
 
 # PHP 7.1
