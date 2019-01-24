906361da94083916164059dfffc6e7555dc2f2cb
Ricardo Seriani <ricardo.seriani@gmail.com>
2019-01-24 10:04:01 -0200

    Ensure that only extract the go folder from golang.tar.gz
    In release 1.11.5 the golang.tar.gz for some architectures contains others folders than go (see: https://github.com/golang/go/issues/29906)
    
    Signed-off-by: Ricardo Seriani <ricardo.seriani@gmail.com>

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -432,7 +432,7 @@ chown vagrant:vagrant /home/vagrant/.zshrc
 
 golangVersion="1.11.5"
 wget https://dl.google.com/go/go${golangVersion}.linux-amd64.tar.gz -O golang.tar.gz
-tar -C /usr/local -xzf golang.tar.gz
+tar -C /usr/local -xzf golang.tar.gz go
 printf "\nPATH=\"/usr/local/go/bin:\$PATH\"\n" | tee -a /home/vagrant/.profile
 rm -rf golang.tar.gz
 
