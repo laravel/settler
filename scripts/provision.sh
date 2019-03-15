2664d184e671fb2d4aba0c1788348bbe361bf87a
Ricardo Seriani <ricardo.seriani@gmail.com>
2019-03-15 09:21:10 -0300

    Update golang version to 1.12.1
    Signed-off-by: Ricardo Seriani <ricardo.seriani@gmail.com>

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -431,7 +431,7 @@ chown vagrant:vagrant /home/vagrant/.zshrc
 
 # Install Golang
 
-golangVersion="1.12"
+golangVersion="1.12.1"
 wget https://dl.google.com/go/go${golangVersion}.linux-amd64.tar.gz -O golang.tar.gz
 tar -C /usr/local -xzf golang.tar.gz go
 printf "\nPATH=\"/usr/local/go/bin:\$PATH\"\n" | tee -a /home/vagrant/.profile
