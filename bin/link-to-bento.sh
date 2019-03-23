0c2cd19ac798727f6dbc1c050fbd2bc93683dd5a
Andrew Berry <deviantintegral@gmail.com>
2019-03-23 13:14:03 -0400

    Fix not properly testing empty string

--- bin/link-to-bento.sh
+++ bin/link-to-bento.sh
@@ -1,7 +1,21 @@
-#!/usr/bin/env bash -x
+#!/usr/bin/env bash
+
+SED=sed
+
+uname -a | grep Darwin > /dev/null
+if [ $? -eq 0 ]
+then
+  if [ -x "$(which gsed)" ]
+  then
+    SED=gsed
+  else
+    echo "On macOS this script requires GNU sed. Install it with 'brew install gnu-sed'."
+    exit 1
+  fi
+fi
+
 /bin/ln -f scripts/provision.sh ../bento/ubuntu/scripts/homestead.sh
 /bin/ln -f http/preseed.cfg ../bento/ubuntu/http
 /bin/ln -f http/preseed-hyperv.cfg ../bento/ubuntu/http
 
-[ -x $(which gsed) ] && SED=gsed || SED=sed
 $SED -i 's/scripts\/cleanup.sh/scripts\/homestead.sh/' ../bento/ubuntu/ubuntu-18.04-amd64.json
