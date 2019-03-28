2416f6eb65cf9c4a45f8fc624866cbb26a50dd9a
Andrew Berry <deviantintegral@gmail.com>
2019-03-28 16:51:36 -0400

    Use sed -e for BSD sed compatibility

--- bin/link-to-bento.sh
+++ bin/link-to-bento.sh
@@ -1,21 +1,7 @@
 #!/usr/bin/env bash
 
-SED=sed
-
-uname -a | grep Darwin > /dev/null
-if [ $? -eq 0 ]
-then
-  if [ -x "$(which gsed)" ]
-  then
-    SED=gsed
-  else
-    echo "On macOS this script requires GNU sed. Install it with 'brew install gnu-sed'."
-    exit 1
-  fi
-fi
-
 /bin/ln -f scripts/provision.sh ../bento/ubuntu/scripts/homestead.sh
 /bin/ln -f http/preseed.cfg ../bento/ubuntu/http
 /bin/ln -f http/preseed-hyperv.cfg ../bento/ubuntu/http
 
-$SED -i 's/scripts\/cleanup.sh/scripts\/homestead.sh/' ../bento/ubuntu/ubuntu-18.04-amd64.json
+sed -i -e 's/scripts\/cleanup.sh/scripts\/homestead.sh/' ../bento/ubuntu/ubuntu-18.04-amd64.json
