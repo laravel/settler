07c42126376903f2e4f650632e4e67578de90489
Joe Ferguson <joe@joeferguson.me>
2019-06-30 12:58:37 -0500

    Fix sed command

--- bin/link-to-bento.sh
+++ bin/link-to-bento.sh
@@ -4,7 +4,7 @@
 /bin/ln -f http/preseed.cfg ../bento/packer_templates/ubuntu/http
 /bin/ln -f http/preseed-hyperv.cfg ../bento/packer_templates/ubuntu/http
 
-sed -i -e 's/scripts\/cleanup.sh/scripts\/homestead.sh/' ../bento/packer_templates/ubuntu/ubuntu-18.04-amd64.json
-sed -i -e 's/"cpus": "1"/"cpus": "4"/' ../bento/packer_templates/ubuntu/ubuntu-18.04-amd64.json
-sed -i -e 's/"memory": "1024"/"memory": "4096"/' ../bento/packer_templates/ubuntu/ubuntu-18.04-amd64.json
-sed -i -e 's/"disk_size": "65536"/"disk_size": "131072"/' ../bento/packer_templates/ubuntu/ubuntu-18.04-amd64.json
+sed -i '' 's/scripts\/cleanup.sh/scripts\/homestead.sh/' ../bento/packer_templates/ubuntu/ubuntu-18.04-amd64.json
+sed -i '' 's/"cpus": "1"/"cpus": "4"/' ../bento/packer_templates/ubuntu/ubuntu-18.04-amd64.json
+sed -i '' 's/"memory": "1024"/"memory": "4096"/' ../bento/packer_templates/ubuntu/ubuntu-18.04-amd64.json
+sed -i '' 's/"disk_size": "65536"/"disk_size": "131072"/' ../bento/packer_templates/ubuntu/ubuntu-18.04-amd64.json
