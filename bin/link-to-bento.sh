56631598214916bd1a96f4982ee8e9b87fd6b361
Joe Ferguson <joe@joeferguson.me>
2019-07-22 19:38:30 -0500

    Remove Bento motd, lower default ram & cpu numbers

--- bin/link-to-bento.sh
+++ bin/link-to-bento.sh
@@ -5,6 +5,7 @@
 /bin/ln -f http/preseed-hyperv.cfg ../bento/packer_templates/ubuntu/http
 
 sed -i '' 's/scripts\/cleanup.sh/scripts\/homestead.sh/' ../bento/packer_templates/ubuntu/ubuntu-18.04-amd64.json
-sed -i '' 's/"cpus": "1"/"cpus": "4"/' ../bento/packer_templates/ubuntu/ubuntu-18.04-amd64.json
-sed -i '' 's/"memory": "1024"/"memory": "4096"/' ../bento/packer_templates/ubuntu/ubuntu-18.04-amd64.json
+sed -i '' 's/"cpus": "1"/"cpus": "2"/' ../bento/packer_templates/ubuntu/ubuntu-18.04-amd64.json
+sed -i '' 's/"memory": "1024"/"memory": "2048"/' ../bento/packer_templates/ubuntu/ubuntu-18.04-amd64.json
 sed -i '' 's/"disk_size": "65536"/"disk_size": "131072"/' ../bento/packer_templates/ubuntu/ubuntu-18.04-amd64.json
+sed -i '' 's/"..\/_common\/motd.sh",//' ../bento/packer_templates/ubuntu/ubuntu-18.04-amd64.json
