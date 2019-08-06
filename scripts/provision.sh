1635775dcd5016e62ce13089f7f19a2f4bc9eeb4
Joe Ferguson <joe@joeferguson.me>
2019-08-05 19:50:16 -0500

    Cleanup Postgres key addition

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -24,9 +24,7 @@ sudo tee /etc/apt/sources.list.d/pgdg.list <<END
 deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main
 END
 
-wget https://www.postgresql.org/media/keys/ACCC4CF8.asc
-sudo apt-key add ACCC4CF8.asc
-rm -rf ACCC4CF8.asc
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
 # Update Package Lists
 apt-get update
