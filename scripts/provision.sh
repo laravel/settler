eb8bbf0dfb51cdf022f9f5b9678631caba7894c6
Joe Ferguson <joe@joeferguson.me>
2019-11-29 12:33:07 -0600

    WIP: Version 9 Updates  (#210)
    * Update all PHP installs w/ known extensions
    
    * explicitly set spark-installer version (#211)
    
    it looks like previously this was switched from `~2.0` to `dev-master`, maybe because there was a needed feature that wasn't released?
    
    anyway, now we're up to version 3, so unless there's a reason we need dev-master, switch back?
    
    (super confident PR, right?)
    
    * Replace NTP with Chrony (#212)
    
    NTP does not handle the Sleep and Hibernate states on the *host* gracefully;
    the clock on the guest becomes stale and is not updated regularly (if ever).
    
    Replacing NTP with Chrony fixes the issue, provided chronyd is configured
    to allow unlimited catching-up via "makestep 1 -1" (the second value
    represents unlimited catch-up, which is necessary when the host has been
    sleeping or hibernating for any period of time longer than a few seconds).
    
    Resolves https://github.com/laravel/homestead/issues/1300
    
    * Remove duplicate `php-fpm` installs. (#213)
    
    Remove `php-fpm` packages installation from Nginx section as they are
    now installed with the PHP related packages.
    
    * Add composer registry manager, give Spark install version

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -31,7 +31,7 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-
 apt-get update
 
 # Install Some Basic Packages
-apt-get install -y build-essential dos2unix gcc git libmcrypt4 libpcre3-dev libpng-dev ntp unzip make python2.7-dev \
+apt-get install -y build-essential dos2unix gcc git libmcrypt4 libpcre3-dev libpng-dev chrony unzip make python2.7-dev \
 python-pip re2c supervisor unattended-upgrades whois vim libnotify-bin pv cifs-utils mcrypt bash-completion zsh \
 graphviz avahi-daemon tshark imagemagick
 
@@ -41,43 +41,54 @@ ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 # Install PHP Stuffs
 # Current PHP
 apt-get install -y --allow-change-held-packages \
-php7.3-cli php7.3-bcmath php7.3-curl php7.3-dev php7.3-gd php7.3-imap php7.3-intl  php7.3-json  php7.3-ldap \
-php7.3-mbstring php7.3-mysql php7.3-odbc php7.3-pgsql php7.3-phpdbg php7.3-pspell php7.3-soap php7.3-sqlite3 \
-php7.3-xml php7.3-zip php7.3-readline php-memcached php-xdebug php-pear php-imagick php-redis
+php7.3 php7.3-bcmath php7.3-bz2 php7.3-cgi php7.3-cli php7.3-common php7.3-curl php7.3-dba php7.3-dev \
+php7.3-enchant php7.3-fpm php7.3-gd php7.3-gmp php7.3-imap php7.3-interbase php7.3-intl php7.3-json php7.3-ldap \
+php7.3-mbstring php7.3-mysql php7.3-odbc php7.3-opcache php7.3-pgsql php7.3-phpdbg php7.3-pspell php7.3-readline \
+php7.3-recode php7.3-snmp php7.3-soap php7.3-sqlite3 php7.3-sybase php7.3-tidy php7.3-xml php7.3-xmlrpc php7.3-xsl \
+php7.3-zip
 
 # PHP 7.4
 apt-get install -y --allow-change-held-packages \
-php7.4-cli php7.4-bcmath php7.4-curl php7.4-dev php7.4-gd php7.4-imap php7.4-intl  php7.4-json  php7.4-ldap \
-php7.4-mbstring php7.4-mysql php7.4-odbc php7.4-pgsql php7.4-phpdbg php7.4-pspell php7.4-soap php7.4-sqlite3 \
-php7.4-xml php7.4-zip php7.4-readline
+php7.4 php7.4-bcmath php7.4-bz2 php7.4-cgi php7.4-cli php7.4-common php7.4-curl php7.4-dba php7.4-dev \
+php7.4-enchant php7.4-fpm php7.4-gd php7.4-gmp php7.4-imap php7.4-interbase php7.4-intl php7.4-json php7.4-ldap \
+php7.4-mbstring php7.4-mysql php7.4-odbc php7.4-opcache php7.4-pgsql php7.4-phpdbg php7.4-pspell php7.4-readline \
+php7.4-snmp php7.4-soap php7.4-sqlite3 php7.4-sybase php7.4-tidy php7.4-xml php7.4-xmlrpc php7.4-xsl php7.4-zip
 
 # PHP 7.2
 apt-get install -y --allow-change-held-packages \
-php7.2-cli php7.2-bcmath php7.2-curl php7.2-dev php7.2-gd php7.2-imap php7.2-intl  php7.2-json  php7.2-ldap \
-php7.2-mbstring php7.2-mysql php7.2-odbc php7.2-pgsql php7.2-phpdbg php7.2-pspell php7.2-soap php7.2-sqlite3 \
-php7.2-xml php7.2-zip php7.2-readline
+php7.2-bcmath php7.2-bz2 php7.2-dba php7.2-enchant php7.2-fpm php7.2-imap php7.2-interbase php7.2-intl \
+php7.2-mbstring php7.2-phpdbg php7.2-soap php7.2-sybase php7.2-xsl php7.2-zip php7.2-cgi php7.2-cli php7.2-common \
+php7.2-curl php7.2-dev php7.2-gd php7.2-gmp php7.2-json php7.2-ldap php7.2-mysql php7.2-odbc php7.2-opcache \
+php7.2-pgsql php7.2-pspell php7.2-readline php7.2-recode php7.2-snmp php7.2-sqlite3 php7.2-tidy php7.2-xml \
+php7.2-xmlrpc
 
 # PHP 7.1
 apt-get install -y --allow-change-held-packages \
-php7.1-cli php7.1-bcmath php7.1-curl php7.1-dev php7.1-gd php7.1-imap php7.1-intl  php7.1-json  php7.1-ldap \
-php7.1-mbstring php7.1-mysql php7.1-odbc php7.1-pgsql php7.1-phpdbg php7.1-pspell php7.1-soap php7.1-sqlite3 \
-php7.1-xml php7.1-zip php7.1-readline php7.1-mcrypt
+php7.1-bcmath php7.1-bz2 php7.1-cgi php7.1-cli php7.1-common php7.1-curl php7.1-dba php7.1-dev php7.1-enchant \
+php7.1-fpm php7.1-gd php7.1-gmp php7.1-imap php7.1-interbase php7.1-intl php7.1-json php7.1-ldap php7.1-mbstring \
+php7.1-mcrypt php7.1-mysql php7.1-odbc php7.1-opcache php7.1-pgsql php7.1-phpdbg php7.1-pspell php7.1-readline \
+php7.1-recode php7.1-snmp php7.1-soap php7.1-sqlite3 php7.1-sybase php7.1-tidy php7.1-xml php7.1-xmlrpc php7.1-xsl \
+php7.1-zip
 
 # PHP 7.0
 apt-get install -y --allow-change-held-packages \
-php7.0-cli php7.0-bcmath php7.0-curl php7.0-dev php7.0-gd php7.0-imap php7.0-intl  php7.0-json  php7.0-ldap \
-php7.0-mbstring php7.0-mysql php7.0-odbc php7.0-pgsql php7.0-phpdbg php7.0-pspell php7.0-soap php7.0-sqlite3 \
-php7.0-xml php7.0-zip php7.0-readline php7.0-mcrypt
+php7.0-bcmath php7.0-bz2 php7.0-cgi php7.0-cli php7.0-common php7.0-curl php7.0-dba php7.0-dev php7.0-enchant \
+php7.0-fpm php7.0-gd php7.0-gmp php7.0-imap php7.0-interbase php7.0-intl php7.0-json php7.0-ldap php7.0-mbstring \
+php7.0-mcrypt php7.0-mysql php7.0-odbc php7.0-opcache php7.0-pgsql php7.0-phpdbg php7.0-pspell php7.0-readline \
+php7.0-recode php7.0-snmp php7.0-soap php7.0-sqlite3 php7.0-sybase php7.0-tidy php7.0-xml php7.0-xmlrpc php7.0-xsl \
+php7.0-zip
 
 # PHP 5.6
 apt-get install -y --allow-change-held-packages \
-php5.6-cli php5.6-bcmath php5.6-curl php5.6-dev php5.6-gd php5.6-imap php5.6-intl  php5.6-json  php5.6-ldap \
-php5.6-mbstring php5.6-mysql php5.6-odbc php5.6-pgsql php5.6-phpdbg php5.6-pspell php5.6-soap php5.6-sqlite3 \
-php5.6-xml php5.6-zip php5.6-readline php5.6-mcrypt
+php5.6-bcmath php5.6-bz2 php5.6-cgi php5.6-cli php5.6-common php5.6-curl php5.6-dba php5.6-dev php5.6-enchant \
+php5.6-fpm php5.6-gd php5.6-gmp php5.6-imap php5.6-interbase php5.6-intl php5.6-json php5.6-ldap php5.6-mbstring \
+php5.6-mcrypt php5.6-mysql php5.6-odbc php5.6-opcache php5.6-pgsql php5.6-phpdbg php5.6-pspell php5.6-readline \
+php5.6-recode php5.6-snmp php5.6-soap php5.6-sqlite3 php5.6-sybase php5.6-tidy php5.6-xml php5.6-xmlrpc php5.6-xsl \
+php5.6-zip
 
-update-alternatives --set php /usr/bin/php7.3
-update-alternatives --set php-config /usr/bin/php-config7.3
-update-alternatives --set phpize /usr/bin/phpize7.3
+update-alternatives --set php /usr/bin/php7.4
+update-alternatives --set php-config /usr/bin/php-config7.4
+update-alternatives --set phpize /usr/bin/phpize7.4
 
 # Install Composer
 curl -sS https://getcomposer.org/installer | php
@@ -87,9 +98,10 @@ mv composer.phar /usr/local/bin/composer
 sudo su vagrant <<'EOF'
 /usr/local/bin/composer global require hirak/prestissimo
 /usr/local/bin/composer global require "laravel/envoy=^1.6"
-/usr/local/bin/composer global require "laravel/installer=^2.1"
+/usr/local/bin/composer global require "laravel/installer=^3.1"
 /usr/local/bin/composer global require "laravel/lumen-installer=^1.1"
-/usr/local/bin/composer global require "laravel/spark-installer=dev-master"
+/usr/local/bin/composer global require "laravel/spark-installer=^3.1"
+/usr/local/bin/composer global require "slince/composer-registry-manager=^2.0"
 EOF
 
 # Set Some PHP CLI Settings
@@ -98,15 +110,15 @@ sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.4/cli/php.in
 sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.4/cli/php.ini
 sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.4/cli/php.ini
 
-sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.2/cli/php.ini
-sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.2/cli/php.ini
-sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.2/cli/php.ini
-sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.2/cli/php.ini
-
 sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.3/cli/php.ini
 sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.3/cli/php.ini
 sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.3/cli/php.ini
 sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.3/cli/php.ini
+
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.2/cli/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.2/cli/php.ini
+sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.2/cli/php.ini
+sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.2/cli/php.ini
 
 sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.1/cli/php.ini
 sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.1/cli/php.ini
@@ -123,9 +135,9 @@ sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/5.6/cli/php.in
 sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/5.6/cli/php.ini
 sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/5.6/cli/php.ini
 
-# Install Nginx & PHP-FPM
+# Install Nginx
 apt-get install -y --allow-downgrades --allow-remove-essential --allow-change-held-packages \
-nginx php7.1-fpm  php7.4-fpm php7.3-fpm php7.2-fpm php7.0-fpm php5.6-fpm
+nginx
 
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
@@ -137,17 +149,17 @@ touch /home/vagrant/.config/nginx/nginx.conf
 sudo ln -sf /home/vagrant/.config/nginx/nginx.conf /etc/nginx/conf.d/nginx.conf
 
 # Setup Some PHP-FPM Options
+echo "xdebug.remote_enable = 1" >> /etc/php/7.4/mods-available/xdebug.ini
+echo "xdebug.remote_connect_back = 1" >> /etc/php/7.4/mods-available/xdebug.ini
+echo "xdebug.remote_port = 9000" >> /etc/php/7.4/mods-available/xdebug.ini
+echo "xdebug.max_nesting_level = 512" >> /etc/php/7.4/mods-available/xdebug.ini
+echo "opcache.revalidate_freq = 0" >> /etc/php/7.4/mods-available/opcache.ini
+
 echo "xdebug.remote_enable = 1" >> /etc/php/7.3/mods-available/xdebug.ini
 echo "xdebug.remote_connect_back = 1" >> /etc/php/7.3/mods-available/xdebug.ini
 echo "xdebug.remote_port = 9000" >> /etc/php/7.3/mods-available/xdebug.ini
 echo "xdebug.max_nesting_level = 512" >> /etc/php/7.3/mods-available/xdebug.ini
 echo "opcache.revalidate_freq = 0" >> /etc/php/7.3/mods-available/opcache.ini
-
-#echo "xdebug.remote_enable = 1" >> /etc/php/7.4/mods-available/xdebug.ini
-#echo "xdebug.remote_connect_back = 1" >> /etc/php/7.4/mods-available/xdebug.ini
-#echo "xdebug.remote_port = 9000" >> /etc/php/7.4/mods-available/xdebug.ini
-#echo "xdebug.max_nesting_level = 512" >> /etc/php/7.4/mods-available/xdebug.ini
-echo "opcache.revalidate_freq = 0" >> /etc/php/7.4/mods-available/opcache.ini
 
 echo "xdebug.remote_enable = 1" >> /etc/php/7.2/mods-available/xdebug.ini
 echo "xdebug.remote_connect_back = 1" >> /etc/php/7.2/mods-available/xdebug.ini
@@ -173,20 +185,6 @@ echo "xdebug.remote_port = 9000" >> /etc/php/5.6/mods-available/xdebug.ini
 echo "xdebug.max_nesting_level = 512" >> /etc/php/5.6/mods-available/xdebug.ini
 echo "opcache.revalidate_freq = 0" >> /etc/php/5.6/mods-available/opcache.ini
 
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.3/fpm/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.3/fpm/php.ini
-sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.3/fpm/php.ini
-sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.3/fpm/php.ini
-sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.3/fpm/php.ini
-sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/7.3/fpm/php.ini
-sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.3/fpm/php.ini
-
-printf "[openssl]\n" | tee -a /etc/php/7.3/fpm/php.ini
-printf "openssl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | tee -a /etc/php/7.3/fpm/php.ini
-
-printf "[curl]\n" | tee -a /etc/php/7.3/fpm/php.ini
-printf "curl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | tee -a /etc/php/7.3/fpm/php.ini
-
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.4/fpm/php.ini
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.4/fpm/php.ini
 sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.4/fpm/php.ini
@@ -200,6 +198,20 @@ printf "openssl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | tee -a /etc/php
 
 printf "[curl]\n" | tee -a /etc/php/7.4/fpm/php.ini
 printf "curl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | tee -a /etc/php/7.4/fpm/php.ini
+
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.3/fpm/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.3/fpm/php.ini
+sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.3/fpm/php.ini
+sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.3/fpm/php.ini
+sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.3/fpm/php.ini
+sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/7.3/fpm/php.ini
+sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.3/fpm/php.ini
+
+printf "[openssl]\n" | tee -a /etc/php/7.3/fpm/php.ini
+printf "openssl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | tee -a /etc/php/7.3/fpm/php.ini
+
+printf "[curl]\n" | tee -a /etc/php/7.3/fpm/php.ini
+printf "curl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | tee -a /etc/php/7.3/fpm/php.ini
 
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.2/fpm/php.ini
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.2/fpm/php.ini
@@ -287,19 +299,19 @@ EOF
 sed -i "s/user www-data;/user vagrant;/" /etc/nginx/nginx.conf
 sed -i "s/# server_names_hash_bucket_size.*/server_names_hash_bucket_size 64;/" /etc/nginx/nginx.conf
 
-sed -i "s/user = www-data/user = vagrant/" /etc/php/7.3/fpm/pool.d/www.conf
-sed -i "s/group = www-data/group = vagrant/" /etc/php/7.3/fpm/pool.d/www.conf
-
-sed -i "s/listen\.owner.*/listen.owner = vagrant/" /etc/php/7.3/fpm/pool.d/www.conf
-sed -i "s/listen\.group.*/listen.group = vagrant/" /etc/php/7.3/fpm/pool.d/www.conf
-sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.3/fpm/pool.d/www.conf
-
 sed -i "s/user = www-data/user = vagrant/" /etc/php/7.4/fpm/pool.d/www.conf
 sed -i "s/group = www-data/group = vagrant/" /etc/php/7.4/fpm/pool.d/www.conf
 
 sed -i "s/listen\.owner.*/listen.owner = vagrant/" /etc/php/7.4/fpm/pool.d/www.conf
 sed -i "s/listen\.group.*/listen.group = vagrant/" /etc/php/7.4/fpm/pool.d/www.conf
 sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.4/fpm/pool.d/www.conf
+
+sed -i "s/user = www-data/user = vagrant/" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/group = www-data/group = vagrant/" /etc/php/7.3/fpm/pool.d/www.conf
+
+sed -i "s/listen\.owner.*/listen.owner = vagrant/" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/listen\.group.*/listen.group = vagrant/" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.3/fpm/pool.d/www.conf
 
 sed -i "s/user = www-data/user = vagrant/" /etc/php/7.2/fpm/pool.d/www.conf
 sed -i "s/group = www-data/group = vagrant/" /etc/php/7.2/fpm/pool.d/www.conf
@@ -330,9 +342,9 @@ sed -i "s/listen\.group.*/listen.group = vagrant/" /etc/php/5.6/fpm/pool.d/www.c
 sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/5.6/fpm/pool.d/www.conf
 
 service nginx restart
-service php7.2-fpm restart
 service php7.4-fpm restart
 service php7.3-fpm restart
+service php7.2-fpm restart
 service php7.1-fpm restart
 service php7.0-fpm restart
 service php5.6-fpm restart
@@ -528,6 +540,9 @@ dpkg --list \
 
 # Delete obsolete networking
 apt-get -y purge ppp pppconfig pppoeconf
+
+# Configure chronyd to fix clock-drift when VM-host sleeps/hibernates.
+sed -i "s/^makestep.*/makestep 1 -1/" /etc/chrony/chrony.conf
 
 # Delete oddities
 apt-get -y purge popularity-contest installation-report command-not-found command-not-found-data friendly-recovery \
