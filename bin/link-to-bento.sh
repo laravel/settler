8cf78207f05f800ba9388d502ad49cd5d73cb929
Joe Ferguson <joe@joeferguson.me>
2020-01-06 08:40:16 -0600

    Increase disk image to 512GB, Increase LVM Volumes (#215)
    Also fixed a bug with the regex to remove the bento MOTD.

--- bin/link-to-bento.sh
+++ bin/link-to-bento.sh
@@ -7,5 +7,5 @@
 sed -i '' 's/scripts\/cleanup.sh/scripts\/homestead.sh/' ../bento/packer_templates/ubuntu/ubuntu-18.04-amd64.json
 sed -i '' 's/"cpus": "1"/"cpus": "2"/' ../bento/packer_templates/ubuntu/ubuntu-18.04-amd64.json
 sed -i '' 's/"memory": "1024"/"memory": "2048"/' ../bento/packer_templates/ubuntu/ubuntu-18.04-amd64.json
-sed -i '' 's/"disk_size": "65536"/"disk_size": "131072"/' ../bento/packer_templates/ubuntu/ubuntu-18.04-amd64.json
-sed -i '' 's/"..\/_common\/motd.sh",//' ../bento/packer_templates/ubuntu/ubuntu-18.04-amd64.json
+sed -i '' 's/"disk_size": "65536"/"disk_size": "524288"/' ../bento/packer_templates/ubuntu/ubuntu-18.04-amd64.json
+sed -i '' '/\/_common\/motd.sh/d' ../bento/packer_templates/ubuntu/ubuntu-18.04-amd64.json
