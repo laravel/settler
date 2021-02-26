20c1fd3bd1850bc4360fa14e6ad76f067ae44c7b
Joe Ferguson <joe@joeferguson.me>
2021-02-25 21:12:26 -0600

    Disable Ubuntu from going to sleep

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -685,6 +685,9 @@ find /var/cache -type f -exec rm -rf {} \;
 # delete any logs that have built up during the install
 find /var/log/ -name *.log -exec rm -f {} \;
 
+# Disable sleep https://github.com/laravel/homestead/issues/1624
+systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+
 # What are you doing Ubuntu?
 # https://askubuntu.com/questions/1250974/user-root-cant-write-to-file-in-tmp-owned-by-someone-else-in-20-04-but-can-in
 sysctl fs.protected_regular=0
