0774f5f095a245fc0dec91d0df439c1ba5cd558b
Joe Ferguson <joe@joeferguson.me>
2022-10-02 19:07:35 -0500

    Add PHP 8.2, set up and use ~/.homestead=features

--- scripts/amd64.sh
+++ scripts/amd64.sh
@@ -65,6 +65,9 @@ curl \
     -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
+# Configure feature tracking path
+mkdir -p /home/vagrant/.homestead-features
+
 if "$SKIP_PHP"; then
   echo "SKIP_PHP is being used, so we're not installing PHP"
 else
@@ -115,6 +118,8 @@ else
   sed -i "s/listen\.group.*/listen.group = vagrant/" /etc/php/5.6/fpm/pool.d/www.conf
   sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/5.6/fpm/pool.d/www.conf
 
+  touch /home/vagrant/.homestead-features/php56
+
   # PHP 7.0
   apt-get install -y --allow-change-held-packages \
   php7.0-bcmath php7.0-bz2 php7.0-cgi php7.0-cli php7.0-common php7.0-curl php7.0-dba php7.0-dev php7.0-enchant \
@@ -157,6 +162,8 @@ else
   sed -i "s/listen\.group.*/listen.group = vagrant/" /etc/php/7.0/fpm/pool.d/www.conf
   sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.0/fpm/pool.d/www.conf
 
+  touch /home/vagrant/.homestead-features/php70
+
   # PHP 7.1
   apt-get install -y --allow-change-held-packages \
   php7.1-bcmath php7.1-bz2 php7.1-cgi php7.1-cli php7.1-common php7.1-curl php7.1-dba php7.1-dev php7.1-enchant \
@@ -198,6 +205,8 @@ else
   sed -i "s/listen\.owner.*/listen.owner = vagrant/" /etc/php/7.1/fpm/pool.d/www.conf
   sed -i "s/listen\.group.*/listen.group = vagrant/" /etc/php/7.1/fpm/pool.d/www.conf
   sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.1/fpm/pool.d/www.conf
+
+  touch /home/vagrant/.homestead-features/php71
 
   # PHP 7.2
   apt-get install -y --allow-change-held-packages \
@@ -242,6 +251,8 @@ else
   sed -i "s/listen\.group.*/listen.group = vagrant/" /etc/php/7.2/fpm/pool.d/www.conf
   sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.2/fpm/pool.d/www.conf
 
+  touch /home/vagrant/.homestead-features/php72
+
   # PHP 7.3
   apt-get install -y --allow-change-held-packages \
   php7.3 php7.3-bcmath php7.3-bz2 php7.3-cgi php7.3-cli php7.3-common php7.3-curl php7.3-dba php7.3-dev php7.3-enchant \
@@ -283,6 +294,8 @@ else
   sed -i "s/listen\.owner.*/listen.owner = vagrant/" /etc/php/7.3/fpm/pool.d/www.conf
   sed -i "s/listen\.group.*/listen.group = vagrant/" /etc/php/7.3/fpm/pool.d/www.conf
   sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.3/fpm/pool.d/www.conf
+
+  touch /home/vagrant/.homestead-features/php73
 
   # PHP 7.4
   apt-get install -y --allow-change-held-packages \
@@ -326,6 +339,8 @@ else
   sed -i "s/listen\.group.*/listen.group = vagrant/" /etc/php/7.4/fpm/pool.d/www.conf
   sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.4/fpm/pool.d/www.conf
 
+  touch /home/vagrant/.homestead-features/php74
+
   # PHP 8.0
   apt-get install -y --allow-change-held-packages \
   php8.0 php8.0-bcmath php8.0-bz2 php8.0-cgi php8.0-cli php8.0-common php8.0-curl php8.0-dba php8.0-dev \
@@ -367,6 +382,8 @@ else
   sed -i "s/listen\.owner.*/listen.owner = vagrant/" /etc/php/8.0/fpm/pool.d/www.conf
   sed -i "s/listen\.group.*/listen.group = vagrant/" /etc/php/8.0/fpm/pool.d/www.conf
   sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/8.0/fpm/pool.d/www.conf
+
+  touch /home/vagrant/.homestead-features/php80
 
   # PHP 8.1
   apt-get install -y --allow-change-held-packages \
@@ -410,6 +427,53 @@ else
   sed -i "s/listen\.group.*/listen.group = vagrant/" /etc/php/8.1/fpm/pool.d/www.conf
   sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/8.1/fpm/pool.d/www.conf
 
+  touch /home/vagrant/.homestead-features/php8.1
+
+  # PHP 8.2
+  apt-get install -y --allow-change-held-packages \
+  php8.2 php8.2-bcmath php8.2-bz2 php8.2-cgi php8.2-cli php8.2-common php8.2-curl php8.2-dba php8.2-dev \
+  php8.2-enchant php8.2-fpm php8.2-gd php8.2-gmp php8.2-imap php8.2-interbase php8.2-intl php8.2-ldap \
+  php8.2-mbstring php8.2-mysql php8.2-odbc php8.2-opcache php8.2-pgsql php8.2-phpdbg php8.2-pspell php8.2-readline \
+  php8.2-snmp php8.2-soap php8.2-sqlite3 php8.2-sybase php8.2-tidy php8.2-xml php8.2-xsl \
+  php8.2-zip
+  # php8.2-imagick php8.2-memcached php8.2-redis php8.2-xmlrpc php8.2-xdebug
+
+  # Configure php.ini for CLI
+  sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/8.2/cli/php.ini
+  sed -i "s/display_errors = .*/display_errors = On/" /etc/php/8.2/cli/php.ini
+  sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/8.2/cli/php.ini
+  sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/8.2/cli/php.ini
+
+  # Configure Xdebug
+  # echo "xdebug.mode = debug" >> /etc/php/8.2/mods-available/xdebug.ini
+  # echo "xdebug.discover_client_host = true" >> /etc/php/8.2/mods-available/xdebug.ini
+  # echo "xdebug.client_port = 9003" >> /etc/php/8.2/mods-available/xdebug.ini
+  # echo "xdebug.max_nesting_level = 512" >> /etc/php/8.2/mods-available/xdebug.ini
+  # echo "opcache.revalidate_freq = 0" >> /etc/php/8.2/mods-available/opcache.ini
+
+  # Configure php.ini for FPM
+  sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/8.2/fpm/php.ini
+  sed -i "s/display_errors = .*/display_errors = On/" /etc/php/8.2/fpm/php.ini
+  sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/8.2/fpm/php.ini
+  sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/8.2/fpm/php.ini
+  sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/8.2/fpm/php.ini
+  sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/8.2/fpm/php.ini
+  sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/8.2/fpm/php.ini
+
+  printf "[openssl]\n" | tee -a /etc/php/8.2/fpm/php.ini
+  printf "openssl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | tee -a /etc/php/8.2/fpm/php.ini
+  printf "[curl]\n" | tee -a /etc/php/8.2/fpm/php.ini
+  printf "curl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | tee -a /etc/php/8.2/fpm/php.ini
+
+  # Configure FPM
+  sed -i "s/user = www-data/user = vagrant/" /etc/php/8.2/fpm/pool.d/www.conf
+  sed -i "s/group = www-data/group = vagrant/" /etc/php/8.2/fpm/pool.d/www.conf
+  sed -i "s/listen\.owner.*/listen.owner = vagrant/" /etc/php/8.2/fpm/pool.d/www.conf
+  sed -i "s/listen\.group.*/listen.group = vagrant/" /etc/php/8.2/fpm/pool.d/www.conf
+  sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/8.2/fpm/pool.d/www.conf
+
+  touch /home/vagrant/.homestead-features/php8.2
+
   # Disable old PHP FPM
   systemctl disable php5.6-fpm
   systemctl disable php7.0-fpm
@@ -418,6 +482,7 @@ else
   systemctl disable php7.3-fpm
   systemctl disable php7.4-fpm
   systemctl disable php8.0-fpm
+  systemctl disable php8.2-fpm
 
   update-alternatives --set php /usr/bin/php8.1
   update-alternatives --set php-config /usr/bin/php-config8.1
