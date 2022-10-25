1be94d85982314d9c39e066c4c008dff6925c982
Joe Ferguson <joe@joeferguson.me>
2022-10-25 18:47:39 -0500

    Update docker compose

--- scripts/arm.sh
+++ scripts/arm.sh
@@ -61,7 +61,7 @@ usermod -aG docker vagrant
 
 # Install docker-compose
 curl \
-    -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" \
+    -L "https://github.com/docker/compose/releases/download/2.10.1/docker-compose-$(uname -s)-$(uname -m)" \
     -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
