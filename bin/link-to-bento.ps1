9adf882115fc9b85bde067824c2bc6ae3261e162
Michael Nightingale <9887246+mnightingale@users.noreply.github.com>
2023-01-11 13:35:28 +0000

    Add -NoNewLine to each Set-Content

--- bin/link-to-bento.ps1
+++ bin/link-to-bento.ps1
@@ -7,15 +7,15 @@ New-Item -Path ..\bento\packer_templates\ubuntu\http\preseed.cfg -ItemType Symbo
 New-Item -Path ..\bento\packer_templates\ubuntu\http\preseed-hyperv.cfg -ItemType SymbolicLink -Value .\http\preseed-hyperv.cfg -Force
 
 # AMD64
-((Get-Content -path ..\bento\packer_templates\ubuntu\ubuntu-20.04-amd64.json -Raw) -replace 'scripts/cleanup.sh', 'scripts/homestead.sh') | Set-Content -Path ..\bento\packer_templates\ubuntu\ubuntu-20.04-amd64.json
-((Get-Content -path ..\bento\packer_templates\ubuntu\ubuntu-20.04-amd64.json -Raw) -replace '"memory": "1024"', '"memory": "2084"') | Set-Content -Path ..\bento\packer_templates\ubuntu\ubuntu-20.04-amd64.json
-((Get-Content -path ..\bento\packer_templates\ubuntu\ubuntu-20.04-amd64.json -Raw) -replace '"disk_size": "65536"', '"disk_size": "131072"') | Set-Content -Path ..\bento\packer_templates\ubuntu\ubuntu-20.04-amd64.json
-((Get-Content -path ..\bento\packer_templates\ubuntu\ubuntu-20.04-amd64.json -Raw) -replace '"boot_wait": "5s"', '"boot_wait": "3s"') | Set-Content -Path ..\bento\packer_templates\ubuntu\ubuntu-20.04-amd64.json
-((Get-Content -path ..\bento\packer_templates\ubuntu\ubuntu-20.04-amd64.json -Raw) -replace '"type": "hyperv-iso",', '"type": "hyperv-iso","configuration_version": "9.0",') | Set-Content -Path ..\bento\packer_templates\ubuntu\ubuntu-20.04-amd64.json
+((Get-Content -path ..\bento\packer_templates\ubuntu\ubuntu-20.04-amd64.json -Raw) -replace 'scripts/cleanup.sh', 'scripts/homestead.sh') | Set-Content -NoNewline -Path ..\bento\packer_templates\ubuntu\ubuntu-20.04-amd64.json
+((Get-Content -path ..\bento\packer_templates\ubuntu\ubuntu-20.04-amd64.json -Raw) -replace '"memory": "1024"', '"memory": "2084"') | Set-Content -NoNewline -Path ..\bento\packer_templates\ubuntu\ubuntu-20.04-amd64.json
+((Get-Content -path ..\bento\packer_templates\ubuntu\ubuntu-20.04-amd64.json -Raw) -replace '"disk_size": "65536"', '"disk_size": "131072"') | Set-Content -NoNewline -Path ..\bento\packer_templates\ubuntu\ubuntu-20.04-amd64.json
+((Get-Content -path ..\bento\packer_templates\ubuntu\ubuntu-20.04-amd64.json -Raw) -replace '"boot_wait": "5s"', '"boot_wait": "3s"') | Set-Content -NoNewline -Path ..\bento\packer_templates\ubuntu\ubuntu-20.04-amd64.json
+((Get-Content -path ..\bento\packer_templates\ubuntu\ubuntu-20.04-amd64.json -Raw) -replace '"type": "hyperv-iso",', '"type": "hyperv-iso","configuration_version": "9.0",') | Set-Content -NoNewline -Path ..\bento\packer_templates\ubuntu\ubuntu-20.04-amd64.json
 
 # ARM64
-((Get-Content -path ..\bento\packer_templates\ubuntu\ubuntu-20.04-arm64.json -Raw) -replace 'scripts/cleanup.sh', 'scripts/homestead-arm.sh') | Set-Content -Path ..\bento\packer_templates\ubuntu\ubuntu-20.04-arm64.json
-((Get-Content -path ..\bento\packer_templates\ubuntu\ubuntu-20.04-arm64.json -Raw) -replace '"memory": "1024"', '"memory": "2084"') | Set-Content -Path ..\bento\packer_templates\ubuntu\ubuntu-20.04-arm64.json
-((Get-Content -path ..\bento\packer_templates\ubuntu\ubuntu-20.04-arm64.json -Raw) -replace '"disk_size": "65536"', '"disk_size": "131072"') | Set-Content -Path ..\bento\packer_templates\ubuntu\ubuntu-20.04-arm64.json
-((Get-Content -path ..\bento\packer_templates\ubuntu\ubuntu-20.04-arm64.json -Raw) -replace '"boot_wait": "5s"', '"boot_wait": "3s"') | Set-Content -Path ..\bento\packer_templates\ubuntu\ubuntu-20.04-arm64.json
-((Get-Content -path ..\bento\packer_templates\ubuntu\ubuntu-20.04-arm64.json -Raw) -replace '"type": "hyperv-iso",', '"type": "hyperv-iso","configuration_version": "9.0",') | Set-Content -Path ..\bento\packer_templates\ubuntu\ubuntu-20.04-arm64.json
+((Get-Content -path ..\bento\packer_templates\ubuntu\ubuntu-20.04-arm64.json -Raw) -replace 'scripts/cleanup.sh', 'scripts/homestead-arm.sh') | Set-Content -NoNewline -Path ..\bento\packer_templates\ubuntu\ubuntu-20.04-arm64.json
+((Get-Content -path ..\bento\packer_templates\ubuntu\ubuntu-20.04-arm64.json -Raw) -replace '"memory": "1024"', '"memory": "2084"') | Set-Content -NoNewline -Path ..\bento\packer_templates\ubuntu\ubuntu-20.04-arm64.json
+((Get-Content -path ..\bento\packer_templates\ubuntu\ubuntu-20.04-arm64.json -Raw) -replace '"disk_size": "65536"', '"disk_size": "131072"') | Set-Content -NoNewline -Path ..\bento\packer_templates\ubuntu\ubuntu-20.04-arm64.json
+((Get-Content -path ..\bento\packer_templates\ubuntu\ubuntu-20.04-arm64.json -Raw) -replace '"boot_wait": "5s"', '"boot_wait": "3s"') | Set-Content -NoNewline -Path ..\bento\packer_templates\ubuntu\ubuntu-20.04-arm64.json
+((Get-Content -path ..\bento\packer_templates\ubuntu\ubuntu-20.04-arm64.json -Raw) -replace '"type": "hyperv-iso",', '"type": "hyperv-iso","configuration_version": "9.0",') | Set-Content -NoNewline -Path ..\bento\packer_templates\ubuntu\ubuntu-20.04-arm64.json
