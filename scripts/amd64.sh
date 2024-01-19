be20e1f5961167bf4ba6daa2371e93facaccef03
Andrew Brown <browner12@gmail.com>
2024-01-19 13:23:57 -0600

    switch to new Debian keysigning method (#252)
    `apt-key` is deprecated, so we'll switch to the new recommended way of adding repo signing keys
    
    also very minor, but move then creation of the `/etc/apt/keyrings` directory into its own section, as it's not really part of just Node now.

--- scripts/amd64.sh
+++ scripts/amd64.sh
@@ -32,16 +32,16 @@ ca-certificates
 # Install Some PPAs
 apt-add-repository ppa:ondrej/php -y
 
-# NodeJS
+# Prepare keyrings directory
 sudo mkdir -p /etc/apt/keyrings
+
+# NodeJS
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_21.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
 # PostgreSQL
-sudo sh -c 'echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-# Import the repository signing key:
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ACCC4CF8
-wget -qO- https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo tee /etc/apt/trusted.gpg.d/pgdg.asc &>/dev/null
+curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --dearmor -o /etc/apt/keyrings/postgresql.gpg
+sudo sh -c 'echo "deb [signed-by=/etc/apt/keyrings/postgresql.gpg] https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
 ## Update Package Lists
 apt-get update -y
