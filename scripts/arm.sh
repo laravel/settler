ce1cf3387058cd2576c957b8ce02537e0ea24206
Andrew Brown <browner12@gmail.com>
2024-01-19 11:20:23 -0600

    remove temporary variable
    it looks like this was mostly copied from the documentation, so I think we're okay to remove the temporary variable and simplify this a little.

--- scripts/arm.sh
+++ scripts/arm.sh
@@ -35,8 +35,7 @@ apt-add-repository ppa:ondrej/php -y
 # NodeJS
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=21
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_21.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
 # PostgreSQL
 sudo sh -c 'echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
